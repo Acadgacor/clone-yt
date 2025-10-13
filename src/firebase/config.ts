29737e06fa42d7ff4ec72a45a8d2dadd0e413554
Rasyad aziz <wdy5612@gmail.com>
2025-10-13 15:47:12 +0000

    taruh API yang tadi di file env

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-7913045011-dfd1c",
-  "appId": "1:999369271943:web:6f1cd88d53be2fd4822622",
-  "apiKey": "AIzaSyBkTtaYWgZX7ZK4QW3emh7sZuLIobDBVUM",
-  "authDomain": "studio-7913045011-dfd1c.firebaseapp.com",
-  "messagingSenderId": "999369271943"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
